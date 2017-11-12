4c9104c2c32fc1fa7d0cc659042ec27be51ae949
ThunderLightning <thunderman369@hotmail.com>
2017-11-12 18:40:04 +0700

    fix korn's file

--- JS/UploadImage.js
+++ JS/UploadImage.js
@@ -51,18 +51,19 @@ $(document).ready( function() {
         return 0;
     }
 
-    window.genInput = function(baseName,type,val,extraClass) {
-        return '<input class="input form-control '+extraClass+'" name="'+baseName+'[]" type="'+type+'" value="'+val+'">';
+    window.genInput = function(ph,baseName,type,val,extraClass) {
+        return '<input class="input form-control '+extraClass+'" name="'+baseName+'[]" type="'+type+'" value="'+val+'" placeholder="'+ph+'">';
     }
 
     window.genBtn = function(label,extraClass) {
-        return '<button class="btn '+extraClass+'" >'+label+'</button></div>';
+        return '<button class="btn '+extraClass+'" id="b2" >'+label+'</button>';
     }
 
     window.genNewRow = function(type,val) {
         switch(type) {
             default:
-                return '<div class="row-fluid record"><div class="span12">'+genInput('field','text',val)+' '+genBtn('-', 'btn-danger remove-parent')+'</div></div>';
+                return '<div class="row record"><div class="col-sm-10">'+genInput('Tel.','field','text',val)+''+genBtn('-', 'btn-danger remove-parent')+'</div></div>';
+                /*return '<div class="row record"><div class="col-sm-10">'+genInput('Tel.','field','text',val)+'</div><div class="col-sm-2">'+genBtn('-','btn-danger remove-parent')+'</div></div>';*/
         }
         console.log('Check your code Im broken...')
         return ''; // should never happen
