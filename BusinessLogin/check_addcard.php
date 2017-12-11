1dbbaa426e589c0214e130d884652af59d56b3cb
pthas <pthas1996@icloud.com>
2017-12-11 10:42:37 +0700

    Update addcard refresh

--- BusinessLogin/check_addcard.php
+++ BusinessLogin/check_addcard.php
@@ -21,30 +21,31 @@ $expiry_year = $_POST['expiry-year'];
 $bank_id = $_POST['bank'];
 $card_no = $creditCard1." ".$creditCard2." ".$creditCard3." ".$creditCard4;
 $type_card = '1';
+/*
 echo $card_no."</br>";
 echo $card_name."</br>";
 echo $expiry_month."</br>";
 echo $expiry_year."</br>";
-
+*/
 //echo substr($creditCard1, 0 , );
 if(strcmp(substr($creditCard1, 0 , 1),"4")==0){
-  echo "-->1";
+  //echo "-->1";
   $type_card = 1;
 }else if(strcmp(substr($creditCard1, 0 , 2),"35")==0){
-  echo "-->2";
+  //echo "-->2";
   $type_card = 3;
 }else if(strcmp(substr($creditCard1, 0 , 2),"34")==0|| strcmp(substr($creditCard1, 0 , 2),"37")==0){
-  echo "-->3";
+  //echo "-->3";
   $type_card = 4;
 }else if(strcmp(substr($creditCard1, 0 , 2),"50")==0 || strcmp(substr($creditCard1, 0 , 2),"51")==0|| strcmp(substr($creditCard1, 0 , 2),"52")==0 || strcmp(substr($creditCard1, 0 , 2),"53")==0 ||
 strcmp(substr($creditCard1, 0 , 2),"54")==0 || strcmp(substr($creditCard1, 0 , 2),"55")==0){
-  echo "-->4";
+  //echo "-->4";
   $type_card = 2;
 }else if(strcmp(substr($creditCard1, 0 , 2),"62")==0){
-  echo "-->5";
+  //echo "-->5";
   $type_card = 5;
 }else{
-  echo "-->6";
+  //echo "-->6";
   echo "<script type=\"text/javascript\">
   alert('card not match');
   </script>";
@@ -56,7 +57,7 @@ strcmp(substr($creditCard1, 0 , 2),"54")==0 || strcmp(substr($creditCard1, 0 , 2
 $sql = "INSERT INTO card (card_no,card_name,exp_m,exp_y,user_id,type_id,bank_id) VALUES ('$card_no' , '$card_name','$expiry_month ','$expiry_year', '$user_id' ,'$type_card' ,'$bank_id')";
   if (mysqli_query($conn, $sql)) {
         echo "<script type='text/javascript'>alert('Edit Successfully');</script>";
-        Header("refresh: 0;AddCard2[Bus].php");
+        Header("refresh: 0;MyCard[Bus].php");
    } else {
          echo "<script>";
          echo "alert('Fail');";
