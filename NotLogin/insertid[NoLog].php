7f22d73ed6fd166b839f6e7c55768630c29bb32d
pthas <pthas1996@icloud.com>
2017-12-11 17:11:47 +0700

    updatebug insert[NoLog]

--- NotLogin/insertid[NoLog].php
+++ NotLogin/insertid[NoLog].php
@@ -19,11 +19,11 @@ $pass = $_POST['pass'];
 $sql = " SELECT * FROM user WHERE user_email ='$mail' ";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result)==0) {
-  $sql = "INSERT INTO user (user_id,user_fname,user_lname,user_email,user_password) VALUES ('' , '$name', '$surname' , '$mail ' , ' $pass ')";
+  $sql = "INSERT INTO user (user_id,user_fname,user_lname,user_email,user_password) VALUES ('' , '$name', '$surname' , '$mail' ,'$pass')";
   if (mysqli_query($conn, $sql)) {
     echo "<script type='text/javascript'>alert('Edit Successfully');</script>";
     Header("refresh: 0;Home[NoLog].php");
-      
+
   } else {
         echo "<script>";
         echo "swal('Fail');";
