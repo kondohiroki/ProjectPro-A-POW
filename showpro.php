c8e769a3443f06fa43bdef4786e97bbcdf77caa8
ThunderLightning <thunderman369@hotmail.com>
2017-12-11 15:36:29 +0700

    Home Subscribe Removed

--- showpro.php
+++ showpro.php
@@ -1,15 +1,14 @@
 <?php
-include '../dbconnect.php';
+include 'dbconnect.php';
 
 $sql = " SELECT * FROM `promotion` ";
 $result = mysqli_query($conn,$sql);
-$row= mysqli_fetch_array($result);
+
 if(mysqli_num_rows($result) > 0){
   while($row = mysqli_fetch_assoc($result)){
     $pro_img =$row['pro_img'];
     $pro_name=$row['pro_name'];
     $pro_des=$row['description'];
-
     $pro_id=$row['pro_id'];
 
     echo '<div class="row">';
@@ -18,8 +17,8 @@ if(mysqli_num_rows($result) > 0){
             echo '<img class="card-img-top" src="data:image/jpeg;base64,'.base64_encode($pro_img).'"/>';
             echo '<div class="card-body">';
               echo "<div data-toggle='collapse' data-target='#".$pro_id."'><h4 class='card-title'>".$pro_name."</h4></div>";
-              echo '<button type="button" class="btn btn-outline-primary" data-toggle="collapse" data-target="#'.$pro_id.'" id="buttProLeft">Details</button>';
-              echo '<a href="#"><button type="button" class="btn btn-outline-danger" id="buttProRight">Subscribe</button></a>';
+              echo '<center><button type="button" class="btn btn-outline-primary" data-toggle="collapse" data-target="#'.$pro_id.'" id="buttProLeft">Details</button></center>';
+              //echo '<a href="#"><button type="button" class="btn btn-outline-danger" id="buttProRight">Subscribe</button></a>';
               echo '<div id="'.$pro_id.'" class="collapse " >';
               echo '</br>';
               echo '<p>'.$pro_des.'<p>';
